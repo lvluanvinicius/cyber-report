0378f439be5a5fbd03380add55449fa0126988fe
Luan Santos <lvluansantos@gmail.com>
2025-10-23 08:16:33 -0300

    feat: create start project

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { wayfinder } from '@laravel/vite-plugin-wayfinder';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.tsx'],
+            ssr: 'resources/js/ssr.tsx',
+            refresh: true,
+        }),
+        react({
+            babel: {
+                plugins: ['babel-plugin-react-compiler'],
+            },
+        }),
+        tailwindcss(),
+        wayfinder({
+            formVariants: true,
+        }),
+    ],
+    esbuild: {
+        jsx: 'automatic',
+    },
+    server: {
+        cors: {
+            origin: [
+                'http://localhost:8005',
+                'http://dev-cyber-report.luxesoftwares.com.br:8005',
+            ]
+        },
+        port: 8005,
+        host: '0.0.0.0',
+        origin: 'http://191.37.38.59:8005'
+    }
+});
